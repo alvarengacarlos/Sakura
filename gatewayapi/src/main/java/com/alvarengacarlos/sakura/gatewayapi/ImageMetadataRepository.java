e256dabfd275a124d296567f48deda2eef8efe79
alvarengacarlos <carlosho200@hotmail.com>
2026-04-20 16:02:39 -0400

    (gatewayapi): Removes duplicated files

--- gatewayapi/src/main/java/com/alvarengacarlos/sakura/gatewayapi/ImageMetadataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.alvarengacarlos.sakura.gatewayapi;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ImageMetadataRepository extends JpaRepository<ImageMetadataEntity, UUID> {
-}
--- gatewayapi/src/main/java/com/alvarengacarlos/sakura/gatewayapi/ImageMetadataStatus.java
+++
@@ -1,6 +0,0 @@
-package com.alvarengacarlos.sakura.gatewayapi;
-
-public enum ImageMetadataStatus {
-    PENDING_ANALYSIS,
-    ANALYZED
-}
