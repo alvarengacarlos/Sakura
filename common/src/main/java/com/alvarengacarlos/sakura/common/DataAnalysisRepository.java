e94b821b51bd27aae5ace902de9ef41657a84619
alvarengacarlos <carlosho200@hotmail.com>
2026-04-22 10:50:04 -0400

    (common): Creates in data analysis repository a method to search data analysis by next purchase date

--- common/src/main/java/com/alvarengacarlos/sakura/common/DataAnalysisRepository.java
+++ common/src/main/java/com/alvarengacarlos/sakura/common/DataAnalysisRepository.java
@@ -2,7 +2,11 @@ package com.alvarengacarlos.sakura.common;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface DataAnalysisRepository extends JpaRepository<DataAnalysisEntity, UUID> {
+
+    List<DataAnalysisEntity> findByNextPurchaseDate(LocalDate date);
 }
