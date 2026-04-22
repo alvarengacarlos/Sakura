8eda2cbfb5f32cba2c8683c7b0535fde0eaf3248
alvarengacarlos <carlosho200@hotmail.com>
2026-04-22 08:11:54 -0400

    (common): Creates in tax receipt repository a method to search tax receipts by transaction time

--- common/src/main/java/com/alvarengacarlos/sakura/common/TaxReceiptRepository.java
+++ common/src/main/java/com/alvarengacarlos/sakura/common/TaxReceiptRepository.java
@@ -2,7 +2,11 @@ package com.alvarengacarlos.sakura.common;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface TaxReceiptRepository extends JpaRepository<TaxReceiptEntity, UUID> {
+
+    List<TaxReceiptEntity> findByTransactionTimeAfter(LocalDateTime dateTime);
 }
